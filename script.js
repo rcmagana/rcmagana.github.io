c7f803bd35dfdef323125f88fcf10a288ee5d925
Raul Magana <raulmagana@Rauls-MacBook-Pro.local>
2018-12-14 12:26:10 -0800

    added content

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.slider');
+        var instances = M.Slider.init(elems);
+      });
+
+      document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.tap-target');
+        var instances = M.TapTarget.init(elems);
+      });
+    
+    
+        
